c251f686ed343cd19bc07f6f122cd7a1970e3b23
Andee Kaplan <andee.kaplan@gmail.com>
2026-08-24 13:43:12 -0600

    fix lab and homework

--- homeworks/README.md
+++ homeworks/README.md
@@ -8,10 +8,13 @@ Complete all homework in rstudio.cloud in the appropriate repo and turn in repor
 
 ## Assignments
 
-1. [Homework 1](https://github.com/dsci445-csu/hw-1/) - Due Sep. 6
-1. [Homework 2](https://github.com/dsci445-csu/hw-2/) - Due Sep. 22
-1. [Homework 3](https://github.com/dsci445-csu/hw-3/) - Due Oct. 6
-1. [Homework 4](https://github.com/dsci445-csu/hw-4/) - Due Oct. 20
-1. [Homework 5](https://github.com/dsci445-csu/hw-5/) - Due Nov. 8
-1. [Homework 6](https://github.com/dsci445-csu/hw-6/) - Due Nov. 22
-1. [Homework 7](https://github.com/dsci445-csu/hw-7/) - Due Dec. 9
+1. [Homework 1](https://github.com/dsci445-csu/hw-1/) - Due Sep. 1
+1. [Homework 2](https://github.com/dsci445-csu/hw-2/) - Due Sep. 8
+1. [Homework 3](https://github.com/dsci445-csu/hw-3/) - Due Sep. 15
+1. [Homework 4](https://github.com/dsci445-csu/hw-4/) - Due Sep. 22
+1. [Homework 5](https://github.com/dsci445-csu/hw-5/) - Due Oct. 6
+1. [Homework 6](https://github.com/dsci445-csu/hw-6/) - Due Oct. 27
+1. [Homework 7](https://github.com/dsci445-csu/hw-7/) - Due Nov. 3
+1. [Homework 7](https://github.com/dsci445-csu/hw-8/) - Due Nov. 10
+1. [Homework 7](https://github.com/dsci445-csu/hw-9/) - Due Nov. 17
+1. [Homework 7](https://github.com/dsci445-csu/hw-10/) - Due Dec. 1
--- labs/scripts/9_svm.R
+++ labs/scripts/9_svm.R
@@ -1,4 +1,5 @@
 library(tidyverse) ## data manipulation
+library(tidymodels) ## models
 library(knitr) ## tables
 
 ## reproducible
@@ -29,17 +30,15 @@ df_large <- data.frame(x_large, y = as.factor(y_large))
 # 1. Make two scatterplots to inspect the small and large training data sets. Describe what you see.
 
 ## Support Vector Classifier
-library(e1071) ## svm library
-# 1. Use the `svm` function to fit a support vector classifier on the small data with $C = 10$ (use `scale = FALSE` to disallow rescaling of your data.
-# 2. Inspect your model using `summary()`. How many support vectors were used to fit your classifier?
-# 3. Predict (`predict()`) a grid of $\boldsymbol X$ values between the range of $X_1$ and $X_2$. Plot these predictions using `geom_tile()` to visualize the decision boundary and add a scatteplot of training data on top, colored by training label. Describe what you see.
+# 1. Fit a support vector classifier on the small data with $C = 10$ (use `scaled = FALSE` to indicated your data has not been scaled.)
+# 2. How many support vectors were used to fit your classifier?
+# 3. Predict a grid of $\boldsymbol X$ values between the range of $X_1$ and $X_2$. Plot these predictions using `geom_tile()` to visualize the decision boundary and add a scatteplot of training data on top, colored by training label. Describe what you see.
 # 4. Alter your plot from 2 to change the shape of the support vectors.
-# 5. Use the `tune()` function to perform CV on the cost parameter. Which value of $C$ would you choose?
+# 5. Perform CV on the cost parameter. Which value of $C$ would you choose?
 # 6. Repeat 3. and 4. using your chosen $C$ value. Describe what you see.
-                                                                                              
+
 ## Support Vector Machines
 # 1. Split the large data frame into 50% training and 50% test.
-# 2. Fit a linear SVM, radial SVM with $\gamma = 1$, and polynomial SVM with $d = 3$ using `tune()` to choose the appropriate cost for each model.
-# 3. Predict (`predict()`) a grid of $\boldsymbol X$ values between the range of $X_1$ and $X_2$. Plot these predictions using `geom_tile()` to visualize the decision boundary and add a scatteplot of training data on top, colored by training label. Describe what you see.
+# 2. Fit a linear SVM, radial SVM with $\gamma = 1$, and polynomial SVM with $d = 3$ using CV to choose the appropriate cost for each model.
+# 3. Predict a grid of $\boldsymbol X$ values between the range of $X_1$ and $X_2$. Plot these predictions using `geom_tile()` to visualize the decision boundary and add a scatteplot of training data on top, colored by training label. Describe what you see.
 # 4. Predict your test data with your three models. Which model would you choose?
-
